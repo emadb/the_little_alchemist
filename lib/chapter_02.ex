f1674e86383b9443f8611a54beff5e18b6586945
Emanuele DelBono <emanuele@codiceplastico.com>
2023-10-29 10:15:25 +0100

    chapter 2

---
+++ lib/chapter_02.ex
@@ -0,0 +1,25 @@
+defmodule LittleAlchemist.Chapter02 do
+
+  # def lat?(lat) do
+  #   cond do
+  #     null?(lat) -> true
+  #     atom?(car(lat)) -> lat?(cdr(lat))
+  #     true -> false
+  #   end
+  # end
+
+  def lat?([]), do: true
+  def lat?([[_|_]|_]), do: false
+  def lat?([_|t]), do: lat?(t)
+
+  # def member?(a, lat) do
+  #   cond do
+  #     null?(lat) -> false
+  #     true -> eq?((car (lat)), a) || member?(a, cdr(lat))
+  #   end
+  # end
+
+  def member?(_, []), do: false
+  def member?(h, [h | _]), do: true
+  def member?(a, [_ | t]), do: member?(a, t)
+end
---
+++ test/chapter_02_test.exs
@@ -0,0 +1,21 @@
+defmodule LittleAlchemist.Chapter02Test do
+  use ExUnit.Case
+  import LittleAlchemist.Chapter02
+
+  test "lat?" do
+    res = lat? [:jack, :sprat, :could, :eat, :no, :chicken]
+    assert res == true
+
+    res = lat? [[:jack], :sprat, :could, :eat, :no, :chicken]
+    assert res == false
+  end
+
+  test "member?" do
+    res = member? :c, [:a, :b, :c, :d]
+    assert res == true
+
+    res = member? :f, [:a, :b, :c, :d]
+    assert res == false
+  end
+
+end
